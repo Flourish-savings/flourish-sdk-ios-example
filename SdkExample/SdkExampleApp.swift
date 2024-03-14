89f2e3e89547e10f2ab896922ebcc36e14880837
Rafael Kerr <rafael@flourishfi.com>
2024-03-14 12:23:21 -0300

    update

--- SdkExample/SdkExampleApp.swift
+++ SdkExample/SdkExampleApp.swift
@@ -1,21 +1,14 @@
-//
-//  SdkExampleApp.swift
-//  SdkExample
-//
-//  Created by Yuri Logatto Pamplona on 10/03/24.
-//
-
 import SwiftUI
 import FlourishSDK
 
 @main
 struct SdkExampleApp: App {
     @StateObject private var flourishSdkManager = FlourishSdkManager(
-        customerCode: "123",
-        partnerUuid: "partnerUuid",
-        partnerSecret: "partnerSecret",
+        customerCode: "578956",
+        partnerUuid: "2476f19f-bf9c-4b52-9c51-a5cbf56fc89e",
+        partnerSecret: "4dc94959-1f7b-434b-bc98-c9cd3c9e3807",
         environment: Environment.staging,
-        language: Language.pt
+        language: Language.en
     )
     
     var body: some Scene {
