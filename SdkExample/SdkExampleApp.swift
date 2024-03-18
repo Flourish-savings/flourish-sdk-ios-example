6852e4fd45b1192ee1d6b5cdd074ae1b8f18caf9
rafaelkerr <87027646+rafaelkerr@users.noreply.github.com>
2024-03-18 16:32:40 -0300

    Update SdkExampleApp.swift

--- SdkExample/SdkExampleApp.swift
+++ SdkExample/SdkExampleApp.swift
@@ -4,9 +4,9 @@ import FlourishSDK
 @main
 struct SdkExampleApp: App {
     @StateObject private var flourishSdkManager = FlourishSdkManager(
-        customerCode: "578956",
-        partnerUuid: "2476f19f-bf9c-4b52-9c51-a5cbf56fc89e",
-        partnerSecret: "4dc94959-1f7b-434b-bc98-c9cd3c9e3807",
+        customerCode: "HERE_YOU_WILL_USE_YOUR_CUSTOMER_CODE",
+        partnerUuid: "HERE_YOU_WILL_USE_YOUR_PARTNER_ID",
+        partnerSecret: "HERE_YOU_WILL_USE_YOUR_SECRET",
         environment: Environment.staging,
         language: Language.en
     )
